b4524e1680d68952799714a60ddbddfa581a882e
Marc Seiferle <marc.seiferle@bluewin.ch>
2021-07-22 14:26:54 +0200

    add new rectangle item cursor

--- src/components/Canvas/Artboard/Artboard.jsx
+++ src/components/Canvas/Artboard/Artboard.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 
-import artboardAtom, { withWidth } from 'recoil/artboard';
 import presetsAtom from 'recoil/presets';
+import artboardAtom, { withWidth } from 'recoil/artboard';
+import { newCanvasItemTypeAtom } from 'recoil/canvas';
 import useSvgRef from 'hooks/useSvgRef';
 
 import Grid from './Grid';
@@ -18,20 +19,39 @@ const Box = styled.div`
   color: ${(props) => props.theme.artboard.color};
 `;
 
+const SvgContainer = styled.svg.attrs({
+  version: '1.1',
+  xmlns: 'http://www.w3.org/2000/svg',
+  xmlnsXlink: 'http://www.w3.org/1999/xlink',
+})`
+  &:hover {
+    cursor: ${({ cursor }) => {
+      switch (cursor) {
+        case 'rectangle':
+          return 'url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcKICAgIGZpbGw9Im5vbmUiCiAgPjxwYXRoIHN0cm9rZT0iIzFlMjQzMyIgZD0iTTE0LjUgMS41aDh2OGgtOHoiIC8+PHBhdGgKICAgICAgc3Ryb2tlPSIjRkZGIgogICAgICBkPSJNMTMuNS41aDEwdjEwaC0xMHoiCiAgICAvPjxwYXRoIHN0cm9rZT0iI0ZGRiIgZD0iTTE1LjUgMi41aDZ2NmgtNnoiIC8+PHBhdGgKICAgICAgZD0iTTcuNSAxMC41djEybS02LTZoMTIiCiAgICAgIHN0cm9rZT0iI0ZGRiIKICAgICAgc3Ryb2tlLXdpZHRoPSIzIgogICAgICBzdHJva2UtbGluZWNhcD0ic3F1YXJlIgogICAgLz48cGF0aAogICAgICBkPSJNNy41IDEwLjV2MTJtLTYtNmgxMiIKICAgICAgc3Ryb2tlPSIjMWUyNDMzIgogICAgICBzdHJva2UtbGluZWNhcD0ic3F1YXJlIgogICAgLz48L2c+PC9zdmc+Cg==) 7.5 17.5, auto';
+        default:
+          return 'initial';
+      }
+    }};
+  }
+`;
+
 const Artboard = ({ children }) => {
   const svg = useSvgRef();
   const { margin } = useRecoilValue(artboardAtom);
   const { iconSize } = useRecoilValue(presetsAtom);
+
   const width = useRecoilValue(withWidth);
+  const newItemType = useRecoilValue(newCanvasItemTypeAtom);
 
   const viewBox = `0 0 ${iconSize} ${iconSize}`;
 
   return (
     <Box width={width} margin={margin}>
-      <svg ref={svg} width={width} height={width} viewBox={viewBox}>
+      <SvgContainer ref={svg} width={width} height={width} viewBox={viewBox} cursor={newItemType}>
         <Grid size={iconSize} />
         {children}
-      </svg>
+      </SvgContainer>
     </Box>
   );
 };
